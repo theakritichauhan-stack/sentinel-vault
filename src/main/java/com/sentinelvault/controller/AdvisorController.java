f7fcdf1c0ee533c65da9582c89da94e5806ff184
Akriti Chauhan <theakritichauhan@gmail.com>
2026-06-25 12:40:05 +0530

    Testing advisor controller

--- src/main/java/com/sentinelvault/controller/AdvisorController.java
+++ src/main/java/com/sentinelvault/controller/AdvisorController.java
@@ -1,36 +1,22 @@
 package com.sentinelvault.controller;
 
-import com.sentinelvault.entity.User;
 import com.sentinelvault.service.AIAdvisorService;
-
-import jakarta.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.ResponseBody;
 
-//@Controller
+@Controller
 public class AdvisorController {
 
     @Autowired
     private AIAdvisorService aiAdvisorService;
 
     @GetMapping("/advisor")
-@ResponseBody
-public String advisorPage(HttpSession session) {
+    @ResponseBody
+    public String advisorPage() {
 
-    User user =
-            (User) session.getAttribute(
-                    "loggedInUser"
-            );
-
-    if(user == null) {
-        return "USER IS NULL";
+        return "Advisor Controller Working";
     }
-
-    return "ADVISOR CONTROLLER WORKING";
-}
 }
