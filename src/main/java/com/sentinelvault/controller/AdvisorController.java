2d82ff142ecdbc2d1bc3995fa02f2bf011bf522b
Akriti Chauhan <theakritichauhan@gmail.com>
2026-06-25 12:49:12 +0530

    revived the changes

--- src/main/java/com/sentinelvault/controller/AdvisorController.java
+++ src/main/java/com/sentinelvault/controller/AdvisorController.java
@@ -1,11 +1,14 @@
 package com.sentinelvault.controller;
 
+import com.sentinelvault.entity.User;
 import com.sentinelvault.service.AIAdvisorService;
+
+import jakarta.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class AdvisorController {
@@ -14,9 +17,14 @@ public class AdvisorController {
     private AIAdvisorService aiAdvisorService;
 
     @GetMapping("/advisor")
-    @ResponseBody
-    public String advisorPage() {
+    public String advisorPage(Model model,
+                              HttpSession session) {
 
-        return "Advisor Controller Working";
+        model.addAttribute("foodTotal", 100);
+        model.addAttribute("rentTotal", 200);
+        model.addAttribute("shoppingTotal", 300);
+        model.addAttribute("transportTotal", 400);
+
+        return "advisor";
     }
 }
